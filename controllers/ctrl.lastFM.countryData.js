61935f8a85cce1342772127b2e27274ca8d7008a
Phil Lucks <lucksp@gmail.com>
2016-03-09 22:48:19 -0700

    navbar styling

--- controllers/ctrl.lastFM.countryData.js
+++ controllers/ctrl.lastFM.countryData.js
@@ -9,56 +9,53 @@ var lfm 			= new LastfmAPI({
 					});
 
 // console.log(matchingCountry.countryName)
-var countryMatches = function (countryName){
-	lfm.geo.getTopTracks({
-		  country: countryName,
-		  limit: 3
-		}, function (err, geoTracks) {
-		if (err) {
-			// console.log(err, name)
-		    return 'OMG!', err, countryName;
-		}
-		else {
-			lfm.chart.getTopTracks({
-				limit: 3
-			},function(err, chartTracks){
-				if(err){
-					// console.log(err)
-					return 'chartTopTracks did not run', err
-				}
-				else {
+// var countryMatches = function (countryName){
+// lfm.chart.getTopTracks({}), function(err, tracks){
+// 			if(err){
+// 				console.log(err)
+// 			}
+			
+	// lfm.geo.getTopTracks({
+	// 	  country: countryName,
+	// 	  limit: 3
+	// 	}, function (err, geoTracks) {
+	// 	if (err) {
+	// 		// console.log(err, name)
+	// 	    return 'OMG!', err, countryName;
+	// 	}
+	// 	else {
+	// 		lfm.chart.getTopTracks({
+	// 			limit: 3
+	// 		},function(err, chartTracks){
+	// 			if(err){
+	// 				// console.log(err)
+	// 				return 'chartTopTracks did not run', err
+	// 			}
+	// 			else {
+	// 				console.log(countryName, 'top Tracks ===', chartTracks.track[0].name, 'has playcount ===', chartTracks.track[0].playcount)
+	// 				// console.log(countryName, 'top Tracks ===', geoTracks.track)
 
-					console.log(countryName, 'top Tracks ===', chartTracks.track[0].name, 'has playcount ===', chartTracks.track[0].playcount)
-					// console.log(countryName, 'top Tracks ===', geoTracks.track)
-
-						// console.log('geoTracks', geoTracks.track[0].name, 'chartTracks', chartTracks.track[0].name)
+	// 					// console.log('geoTracks', geoTracks.track[0].name, 'chartTracks', chartTracks.track[0].name)
 						
-						if (geoTracks.track[0].name === chartTracks.track[0].name || geoTracks.track[1].name === chartTracks.track[1].name || geoTracks.track[2].name === chartTracks.track[2].name)
-						{
-							return
-								country.features.properties.track1 		= geoTracks.track[0].name,
-								country.features.properties.playcount1 	= chartTracks.track[0].playcount,
-								country.features.properties.playLink1 	= geoTracks.track[0].url,
-								country.features.properties.track2 		= geoTracks.track[1].name,
-								country.features.properties.playcount2 	= chartTracks.track[1].playcount,
-								country.features.properties.playLink2 	= geoTracks.track[1].url,
-								country.features.properties.track3 		= geoTracks.track[2].name,
-								country.features.properties.playcount3 	= chartTracks.track[2].playcount,
-								country.features.properties.playLink3 	= geoTracks.track[2].url
-						}
-												// console.log(country.features.properties)
-				}
-			})	
-		}
-	})
-}
+	// 					// if (geoTracks.track[0].name === chartTracks.track[0].name || geoTracks.track[1].name === chartTracks.track[1].name || geoTracks.track[2].name === chartTracks.track[2].name){
+	// 					// 		country.features.properties.track1 		= geoTracks.track[0].name,
+	// 					// 		country.features.properties.playcount1 	= chartTracks.track[0].playcount,
+	// 					// 		country.features.properties.playLink1 	= geoTracks.track[0].url,
+	// 					// 						// console.log(country.features.properties)
+	// 					// }
+	// 				}	
+	// 			}
+	// 		)}
+	// 	})
+	// }
 
 
-for(var i = 0; i < country.features.length; i++){
-		var countryName = country.features[i].properties.name
-		countryMatches(countryName)
-	}
+// for(var i = 0; i < country.features.length; i++){
+// 		var countryName = country.features[i].properties.name
+// 		countryMatches(countryName)
+// 	}
 
-module.exports = {
-	countryTopTracks : countryMatches
-}
+// module.exports = {
+// 	// countryTopTracks : countryMatches
+// 	chartTopTracks : chartTopTracks
+// }
